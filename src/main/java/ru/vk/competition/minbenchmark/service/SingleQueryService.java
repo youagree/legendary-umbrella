e19630bec263d3fc393e552b44148712374228cf
PavelSoldatov <pavsoldatov96@gmail.com>
2022-08-17 11:26:40 +0300

    Add checker

--- src/main/java/ru/vk/competition/minbenchmark/service/SingleQueryService.java
+++ src/main/java/ru/vk/competition/minbenchmark/service/SingleQueryService.java
@@ -1,13 +1,21 @@
 package ru.vk.competition.minbenchmark.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.vk.competition.minbenchmark.dto.AddNewQueryDto;
 import ru.vk.competition.minbenchmark.dto.AddNewQueryResultRequest;
+import ru.vk.competition.minbenchmark.exception.BadRequestException;
+import ru.vk.competition.minbenchmark.feign.SingleQueryFeign;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@RequiredArgsConstructor
 public class SingleQueryService {
+
+    private final SingleQueryFeign queryFeign;
 
     public static ConcurrentHashMap<Integer, AddNewQueryDto> singleQueryStorage = new ConcurrentHashMap<>();
     public static ConcurrentHashMap<Integer, AddNewQueryResultRequest> resultStorage = new ConcurrentHashMap<>();
@@ -17,7 +25,21 @@ public class SingleQueryService {
     }
 
     public void addNewQueryInStorage(Integer resultId, AddNewQueryDto addNewQueryDto) {
-        resultStorage.get(resultId);
+        AddNewQueryResultRequest addNewQueryResultRequest = resultStorage.get(resultId);
 
+        /// TODO: 17.08.2022 НЕПОНЯТНО, потом переделать
+        ResponseEntity<Void> responseEntity = queryFeign.addNewQuery(addNewQueryDto);
+        checkResponse(responseEntity, addNewQueryResultRequest,
+                new BadRequestException("Результат не совпадает"));
+    }
+
+
+    @SneakyThrows
+    private void checkResponse(ResponseEntity responseEntity, AddNewQueryResultRequest expectedResult, Exception e) {
+        int actualStatusCode = responseEntity.getStatusCode().value();
+
+        if (actualStatusCode != expectedResult.getCode()) {
+            throw e;
+        }
     }
 }
