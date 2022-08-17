10f729f3ca1915c7c2ba24e6b327e25216d09148
PavelSoldatov <pavsoldatov96@gmail.com>
2022-08-17 11:14:19 +0300

    Ref storage Add feign

--- src/main/java/ru/vk/competition/minbenchmark/feign/SingleQueryFeign.java
+++ src/main/java/ru/vk/competition/minbenchmark/feign/SingleQueryFeign.java
@@ -1,9 +1,14 @@
 package ru.vk.competition.minbenchmark.feign;
 
+import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.vk.competition.minbenchmark.dto.AddNewQueryDto;
 
-@FeignClient
-public class SingleQueryFeign {
+@FeignClient(name = "/api/single-query")
+public interface SingleQueryFeign {
 
-
+    @PostMapping(value = "/add-new-query")
+    ResponseEntity<Void> addNewQuery(@RequestBody AddNewQueryDto addNewQueryDto);
 }
--- src/main/java/ru/vk/competition/minbenchmark/service/SingleQueryService.java
+++ src/main/java/ru/vk/competition/minbenchmark/service/SingleQueryService.java
@@ -1,6 +1,7 @@
 package ru.vk.competition.minbenchmark.service;
 
 import org.springframework.stereotype.Service;
+import ru.vk.competition.minbenchmark.dto.AddNewQueryDto;
 import ru.vk.competition.minbenchmark.dto.AddNewQueryResultRequest;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,8 +9,15 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class SingleQueryService {
 
-    public static ConcurrentHashMap<Integer, String> singleQueryStorage = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<Integer, AddNewQueryDto> singleQueryStorage = new ConcurrentHashMap<>();
     public static ConcurrentHashMap<Integer, AddNewQueryResultRequest> resultStorage = new ConcurrentHashMap<>();
 
+    public void addNewQueryResult(AddNewQueryResultRequest addNewQueryResultRequest) {
+        resultStorage.put(addNewQueryResultRequest.getResultId(), addNewQueryResultRequest);
+    }
 
+    public void addNewQueryInStorage(Integer resultId, AddNewQueryDto addNewQueryDto) {
+        resultStorage.get(resultId);
+
+    }
 }
